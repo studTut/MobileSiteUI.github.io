becee7e88c378050bdf42e4b8ae1ce0aefe04981
studTut <56622568+studTut@users.noreply.github.com>
2019-10-18 13:35:16 +0900

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,83 @@
+let camera, scene, renderer, cube, material;
+
+const height = window.innerHeight;
+const width = window.innerWidth;
+
+
+function init() {
+	// Init scene
+	scene = new THREE.Scene();
+
+	// Init camera (PerspectiveCamera)
+	camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+
+	// Init renderer
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+
+	// Set size (whole window)
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	// Render to canvas element
+	document.body.appendChild(renderer.domElement);
+
+	// Init BoxGeometry object (rectangular cuboid)
+	const geometry = new THREE.BoxGeometry(3, 3, 3);
+
+	// Create material with color
+	// material = new THREE.MeshBasicMaterial({ color: "hsl(360, 100%, 50%)" });
+	 material = new THREE.MeshNormalMaterial();
+	// material = new THREE.MeshToonMaterial({ color: "hsl(360, 100%, 50%)" });
+	// const material = new THREE.MeshPhongMaterial({ color: 0x6699FF });
+	// material = new THREE.MeshStandardMaterial({ color: "hsl(360, 100%, 50%)" });
+
+	// Add texture - 
+	// const texture = new THREE.TextureLoader().load('textures/crate.gif');
+
+	// Create material with texture
+	// const material = new THREE.MeshBasicMaterial({ map: texture });
+
+	// Create mesh with geo and material
+	cube = new THREE.Mesh(geometry, material);
+	// Add to scene
+	scene.add(cube);
+
+	// Position camera
+	camera.position.z = 5;
+}
+
+// Draw the scene every time the screen is refreshed
+function animate() {
+	requestAnimationFrame(animate);
+
+	// Rotate cube (Change values to change speed)
+	//cube.rotation.x += 0.01;
+	//cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+	// Camera frustum aspect ratio
+	camera.aspect = window.innerWidth / window.innerHeight;
+	// After making changes to aspect
+	camera.updateProjectionMatrix();
+	// Reset size
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    cube.rotation.y = 0.01*(height - window.innerHeight);
+    cube.rotation.x = 0.01*(width - window.innerWidth);
+
+    //material = new THREE.MeshBasicMaterial({ color: "hsl(360, 100%, 100%)" });
+
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
