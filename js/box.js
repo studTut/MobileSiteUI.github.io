12d6705c4d359d4157abfd6a57ed181d162e0184
studTut <56622568+studTut@users.noreply.github.com>
2019-11-29 13:27:10 +0900

    Update box.js

--- js/box.js
+++ js/box.js
@@ -66,7 +66,7 @@ function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	
-    	camera.rotation.x = -20 + beta/40;
+    	camera.rotation.x = -10 + beta/40;
    	camera.rotation.y = gamma/40;
 
 	renderer.render(scene, camera);
