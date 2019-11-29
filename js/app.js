1a2af79a44a7116d069336010541d7e9f8d62cd5
studTut <56622568+studTut@users.noreply.github.com>
2019-11-29 13:41:04 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate);
-    camera.rotation.x = -20 + beta/30;
+    camera.rotation.x = -10 + beta/30;
     camera.rotation.y = gamma/30;
     renderer.render(scene, camera);
 }
