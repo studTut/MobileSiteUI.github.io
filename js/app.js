dfefb06933058bef53c41d64c17ed4fbb2f84e10
studTut <56622568+studTut@users.noreply.github.com>
2020-05-22 15:21:51 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,13 +6,40 @@ var alpha = 0, beta = 0, gamma = 0;
 var line = 200;
 var space = line * 1.1;
 
-
-window.addEventListener("deviceorientation", (dat) => {
-    
+// ジャイロセンサーが有効か？
+if(window.DeviceOrientationEvent){
+    // ★iOS13向け: ユーザーにアクセスの許可を求める関数があるか？
+    if(DeviceOrientationEvent.requestPermission){
+        var sensor_contents= document.getElementById("sensor_contents");
+        // id="sensor_contents" な要素がクリックされたら
+        sensor_contents.addEventListener("click", function(){
+            // ★ジャイロセンサーのアクセス許可をリクエストする
+            DeviceOrientationEvent.requestPermission().then(function(response){
+                // リクエストが許可されたら
+                if(response === "granted"){
+                    // deviceorientationが有効化されるのでaddEventListener
+                    window.addEventListener("deviceorientation", deviceorientationHandler);
+                }
+            }).catch(function(e){
+                console.log(e);
+            });
+        });
+    // iOS13以外
+    }else{
+        // 通常通り、イベントハンドラを追加
+        window.addEventListener("deviceorientation", deviceorientationHandler);
+    }
+}
+ 
+// センサーの値を取得＋値が変わるたびにおこなう処理
+function deviceorientationHandler(){
+    // ※※※※※※※※※※※※※※※※
     alpha = dat.alpha;  // z軸（反時計回り）
     beta  = dat.beta;   // x軸（引き起こす）
     gamma = dat.gamma;  // y軸（右に傾ける）
-});
+    // ※※※※※※※※※※※※※※※※
+}
+
 
 init();
 animate();
@@ -86,4 +113,3 @@ function animate() {
 function render() {
     renderer.render(scene, camera);
 }
-
