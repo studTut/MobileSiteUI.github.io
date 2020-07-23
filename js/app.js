f9a37ef188601908e6fd84f539859c3b42982f40
studTut <56622568+studTut@users.noreply.github.com>
2020-07-23 18:08:32 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -85,7 +85,7 @@ function animate() {
 	//camera.rotation.y = gamma/30;
     object5.rotation.x = 30 -1*beta/15;
     //object5.rotation.y = -1*gamma/15;
-   if(gamma >= 70) { 
+   if(gamma >= 60) { 
 	   window.location.href = '../html/page01.html'; 
    }
 
