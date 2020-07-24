b6b09188d7e8b416569ce51a9a0461acc970a6ce
studTut <56622568+studTut@users.noreply.github.com>
2020-07-24 12:27:21 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function init() {
     object5 = new THREE.CSS3DObject(document.getElementById('num5'));
     scene.add(object5);
     object5.position.x = 0;
-    object5.position.y = space/2;
+    object5.position.y = space;
     object5.position.z = 3.5*space;
 
 
