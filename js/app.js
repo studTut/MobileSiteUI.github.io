32e15b2005477d4dbfdeb7317c0dd0ba433fc923
studTut <56622568+studTut@users.noreply.github.com>
2020-10-13 11:24:40 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -82,7 +82,7 @@ function init() {
     scene.add(object6);
     object6.position.x = 0;
     object6.position.y = 0;
-    object6.position.z = space;
+    object6.position.z = 0;
    // object6.rotation.x = 90;
 	//object6.rotation.z = 90;
 
