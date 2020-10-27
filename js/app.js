791c8b0f36298b642ef4f38969a2740be9f3f0d2
studTut <56622568+studTut@users.noreply.github.com>
2020-10-27 12:33:25 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -124,7 +124,7 @@ function animate() {
     }
     //object5.rotation.y = -1*gamma/15;
     //object6.position.y = scroll;
-    document.querySelector('#num5').style.backgroundColor = 'hsl(10,50%,50%)'
+    document.querySelector('#num5').style.backgroundColor = 'hsl(190,50%,50%)'
 
     renderer.render(scene, camera);
 }
