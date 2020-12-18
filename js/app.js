86595b8c2c5400aa4105df52e5bd8ed8b5e9941b
studTut <56622568+studTut@users.noreply.github.com>
2020-12-19 05:59:29 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ window.addEventListener("deviceorientation", (dat) => {
         initalpha = dat.alpha;  // z軸（反時計回り）
         initbeta  = dat.beta;   // x軸（引き起こす）
         initgamma = dat.gamma;  // y軸（右に傾ける）
-	arg = initbeta + 10;
+	arg = initbeta + 15;
     }, {once : true} );
 init();
 animate();
